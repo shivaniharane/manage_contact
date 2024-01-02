e9536f9b99ac870b5c138a8ef0ff65a32e6a7d75
shivaniharne2 <shivaniharne2@gmail.com>
2024-01-01 19:35:52 -0500

    Complete application setup with frontend, backend, and configurations

---
+++ e_server/e_server/src/test/java/com/e_server/EServerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.e_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootTest
+class EServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
